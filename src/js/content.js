fd08070ea11ec083845981064a5d5a74658cadcd
senpai-10 <bmjfdrh@gmail.com>
2021-06-30 08:00:35 +0300

    fix error

--- src/js/content.js
+++ src/js/content.js
@@ -5,12 +5,14 @@ class Info {
 
   getTags() {
     var tags_list = document.querySelector("#tags > div:nth-child(3) > span") 
-    let list = [] 
-    for (let i = 0; i < tags_list.childElementCount; i++) {
-      var tag = tags_list.children[i].firstChild.textContent.toLowerCase()
-      list.push(tag)
+    if (tags_list) {
+      let list = [] 
+      for (let i = 0; i < tags_list.childElementCount; i++) {
+        var tag = tags_list.children[i].firstChild.textContent.toLowerCase()
+        list.push(tag)
+      }
+      return list
     }
-    return list
   }
 }
 
@@ -21,16 +23,17 @@ function check_tags() {
 
   chrome.storage.sync.get("blacklist",(res) => {
     let blacklist = res.blacklist
-    for (let i = 0; i < get.tags.length; i++) {
-      if (blacklist.includes(get.tags[i])) {
-        detected_tags.push(get.tags[i])
-      }
-    }
+    if (get.tags) {
+      for (let i = 0; i < get.tags.length; i++) {
+        if (blacklist.includes(get.tags[i])) {
+          detected_tags.push(get.tags[i])
+        }
+      } 
 
-    if (detected_tags.length >= 1) {
-      detected_tags = detected_tags.join(" and ")
-      console.log(detected_tags);
-      alert(`warning blacklisted tags detected: ${detected_tags}!`)
+      if (detected_tags.length >= 1) {
+        detected_tags = detected_tags.join(" and ")
+        alert(`warning blacklisted tags detected: ${detected_tags}!`)
+      }
     }
   })
 }
