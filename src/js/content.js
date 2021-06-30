d36d1dad386fe1e9e273014e420607743fd147e5
senpai-10 <bmjfdrh@gmail.com>
2021-06-30 12:40:14 +0300

    findtags method

--- src/js/content.js
+++ src/js/content.js
@@ -1,17 +1,42 @@
 class Info {
   constructor() {
-    this.tags = this.getTags()
+    this.tags = this.findtags()
   }
 
-  getTags() {
-    var tags_list = document.querySelector("#tags > div:nth-child(3) > span") 
-    if (tags_list) {
-      let list = [] 
-      for (let i = 0; i < tags_list.childElementCount; i++) {
-        var tag = tags_list.children[i].firstChild.textContent.toLowerCase()
-        list.push(tag)
+  getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  }
+
+  findtags() {
+    let hostname = location.hostname
+
+    // nhentai
+    if (hostname == "nhentai.net") {
+      var tags_list = document.querySelector("#tags > div:nth-child(3) > span") 
+      if (tags_list) {
+        let list = [] 
+        for (let i = 0; i < tags_list.childElementCount; i++) {
+          var tag = tags_list.children[i].firstChild.textContent.toLowerCase()
+          list.push(tag)
+        }
+        return list
       }
-      return list
+    }
+    
+    if (hostname == "hentaidude.com") {
+      var tags_list = this.getElementByXpath('//*[@id="aboutid"]/div/div[3]')
+      var list = []
+
+      if (tags_list) {
+        for (let i = 0; i < tags_list.childElementCount; i++) {
+            let tag_selector = document.querySelector(`#aboutid > div > div:nth-child(3) > a:nth-child(${i - 1})`)
+          if (tag_selector) {
+            var tag = tag_selector.textContent.toLowerCase()
+            list.push(tag)
+          }
+        }
+        return list
+      }
     }
   }
 }
